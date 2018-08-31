7c34f5e7342a61fcc1373b1e9165b2cebbb0d9bd
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-08-31 16:01:59 -0500

    inputs working, appending to DOM, working on pushing annualSalary to array and adding array

--- client.js
+++ client.js
@@ -2,13 +2,47 @@ console.log('js');
 
 $(document).ready(onReady);
 
+// class Employee {
+//     constructor(firstName, lastName, id, title, annualSalary) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.id = id;
+//         this.title = title;
+//         this.annualSalary = annualSalary;
+//     }
+// }
+
+
 function onReady(){
     console.log('JQ');
     $('#addEmployeeButton').on('click', addEmployee)
     
 }
 
+let salaryArray = [];
+const reducer = accumulator + 
+
 function addEmployee(){
-    console.log('add employee is working');
+    console.log('add employee click is working');
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let id = $('#ID').val();
+    let title = $('#title').val();
+    let annualSalary = $('#annualSalary').val();
+
+    $('#employeeTable').append(`
+        <tr>
+            <td>` + firstName + `</td>
+            <td>` + lastName + `</td>
+            <td>` + id + `</td>
+            <td>` + title + `</td>
+            <td id="salaryInput">` + annualSalary + `</td>
+        </tr>
+    `)
+
+    $('input').val('');
+
+    salaryArray.push((Number(annualSalary)));
+    console.log(salaryArray);
     
 }
